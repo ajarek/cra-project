f978869ec4ef852b36484cb0df61af93f3bf64a8
ajarek <ajarek@poczta.onet.pl>
2022-07-07 22:25:48 +0200

    add responsive

--- src/List.js
+++ src/List.js
@@ -9,6 +9,7 @@ export class List{
  const li = document.createElement('li')
     li.style.listStyleType="none"
     li.style.display="flex"
+    li.style.flexWrap="wrap"
     li.style.justifyContent="space-between"
     li.style.alignItems="center"
     const p = document.createElement('p')
@@ -16,11 +17,13 @@ export class List{
     p.dataset.isCompleted=this.isCompleted
     p.textContent = this.text
     p.style.fontSize="1.3rem"
-    p.style.width="100%"
+    p.style.width="50%"
     p.style.cursor="pointer"
+    const row = document.createElement('div')
     const editBtn = new Button("Edit","#2ecc71",null,this.id,"editBtn")
     const deleteBtn = new Button("Delete","#e74c3c",null,this.id,"deleteBtn")
-    li.append(p,editBtn.render(),deleteBtn.render())
+    row.append(editBtn.render(),deleteBtn.render())
+    li.append(p,row)
     return li
 }
 }
